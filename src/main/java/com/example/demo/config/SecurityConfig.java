59f584a706d5666f2d42b54a2ea630afdb957b98
chvgf <chvgf@naver.com>
2023-09-04 11:09:32 +0900

    9/4 re

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -23,8 +23,7 @@ public class SecurityConfig {
 		.requestMatchers("/member/memberlist").hasAnyRole("ADMIN")
 		.requestMatchers("/item/*").hasAnyRole("ADMIN","USER")
 		.requestMatchers("/board/*").hasAnyRole("ADMIN","USER")
-		.requestMatchers("/notice/*").hasAnyRole("ADMIN","USER")
-		.requestMatchers("/cart/*").hasAnyRole("ADMIN","USER");
+		.requestMatchers("/notice/*").hasAnyRole("ADMIN","USER");
 		
 		http.formLogin();
 		http.csrf().disable();
