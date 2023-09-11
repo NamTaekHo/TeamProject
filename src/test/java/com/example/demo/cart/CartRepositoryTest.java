1b5244bff4b13b779a312086aee100335f401e25
chvgf <chvgf@naver.com>
2023-09-11 13:30:18 +0900

    9/11 wonhee last

--- src/test/java/com/example/demo/cart/CartRepositoryTest.java
+++ src/test/java/com/example/demo/cart/CartRepositoryTest.java
@@ -1,140 +1,86 @@
 package com.example.demo.cart;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.demo.cart.dto.CartDTO;
 import com.example.demo.cart.entity.Cart;
 import com.example.demo.cart.repository.CartRepository;
+import com.example.demo.cart.service.CartService;
 import com.example.demo.item.entity.Item;
 import com.example.demo.item.repository.ItemRepository;
 import com.example.demo.member.entity.Member;
 import com.example.demo.member.repository.MemberRepository;
-
 @SpringBootTest
 public class CartRepositoryTest {
-
 	@Autowired
 	CartRepository cartRepository;
 	
 	@Autowired
-	ItemRepository itemRepository;
+	CartService cartService;
 	
 	@Autowired
 	MemberRepository memberRepository;
 	
+	@Autowired
+	ItemRepository itemRepository;
 	
 	@Test
 	public void 데이터등록() {
+		Optional<Member> result1 = memberRepository.findById("qq");
+		Member m = result1.get();
+		Optional<Item> result2 = itemRepository.findById(1);
+		Optional<Item> result3 = itemRepository.findById(5);
+		Item i1 = result2.get();
+		Item i2 = result3.get();
+		
 		List<Cart> list = new ArrayList<>();
-		Item item1 = Item.builder().itemNo(1).build();
-		Member member1 = Member.builder().id("나무라코").build();		
-		Item item2 = Item.builder().itemNo(2).build();
-		Item item3 = Item.builder().itemNo(3).build();
 		
-		list.add(new Cart(0, item1, member1, 2));
-		list.add(new Cart(0, item2, member1, 3));
-		list.add(new Cart(0, item3, member1, 1));
 		
-		cartRepository.saveAll(list);		
-	}
-	
-	@Test
-	public void 데이터조회() {
-		Optional<Cart> result = cartRepository.findById(4);
-		if(result.isPresent()) {
-			Cart cart = result.get();
-			
-			System.out.println(cart);
-		}
-	}
-	
-	@Test
-	public void 데이터전체조회() {
-		List<Cart> list = cartRepository.findAll();
 		
-		for(int i = 0; i<list.size(); i++) {
-			Cart cart = list.get(i);
-			
-			System.out.println(cart);
-		}
+		list.add(new Cart(0, i2, m, 2));
+		list.add(new Cart(0, i1, m, 1));
+
+		
+		cartRepository.saveAll(list);
+		
+		
 	}
-	
-	@Test
-	public void 데이터수정() {
-		Optional<Cart> result = cartRepository.findById(4);
-		if(result.isPresent()) {
-			Cart cart = result.get();
-			
-			cart.setCount(13);
-			cartRepository.save(cart);
-		}
-	}
-	
-	@Test
-	public void 데이터삭제() {
-		Optional<Cart> result = cartRepository.findById(4);
-		if(result.isPresent()) {
-			Cart cart = result.get();
-			
-			cartRepository.delete(cart);
-		}
-	}
-	
-	
 	
 //	@Test
-//	@DisplayName("장바구니 엔티티를 생성해서 장바구니 리파지토리에 저장에 성공해야한다.")
-//	public void save() {
-//		//given
-//		Item item2 = itemRepository.findById(2).get();
-//		Member aa = memberRepository.findById("aa").get();
+//	public void 쿼리테스트1_dto로표시() {
+//		List<Cart> list = cartRepository.selectCartByID("나무라코");
+//		List<CartDTO> dtolist = new ArrayList<>();
+//		for(int i = 0; i<list.size(); i++) {
+//			Cart c =  list.get(i);
+//			CartDTO dto = cartService.entityToDto(c);
+//			dtolist.add(dto);
+//		}
+//		System.out.println(dtolist);
 //		
-//		Cart cart2 = Cart.builder()
-////		.cartNo(1)
-//		.itemNo(item2)
-//		.id(aa)
-//		.count(1)
-//		.build();
-//		
-//		//when
-//		cartRepository.save(cart2);
-//		
-//		
-////		//then
-////		List<Cart> carts = cartRepository.findAll();
-////		System.out.println(""+carts.size());
-////		
-////		Assertions.assertThat(carts).hasSize(1);
-//		
-//			
 //	}
 	
 //	@Test
-//	public void 아이디별로장바구니목록확인test() {
-//		
-//		List<E>
-//		
+//	public void 쿼리테스트2 () {
+//		Optional<Member> result = memberRepository.findById("나무라코");
+//		Member member = result.get();
+//		List<Cart> list = cartRepository.selectCartByID(member);
+//		System.out.println(list);
+//	}
+	
+//	@Test
+//	public void 쿼리테스트3() {
+//		List<Cart> list = cartRepository.getCartByMemberId("나무라코");
+//		System.out.println(list);
 //	}
 	
 	@Test
-	public void 쿼리테스트1() {
-		List<Cart> list = cartRepository.selectCartByID("나무라코");
-		for(Cart c : list) {
-			System.out.println(c);
+	public void 쿼리테스트4() {
+		List<Cart> list = cartRepository.getCartByMemberId("나무라코");
+		for(Cart cart : list) {
+			System.out.println(cart);
 		}
-		System.out.println();
+//		System.out.println(list);
 	}
-	
-	
-
-
 }
